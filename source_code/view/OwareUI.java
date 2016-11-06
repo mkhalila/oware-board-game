92045456bcdf31fd874559a47af4ea462bc566a0
Khalil Ahmed <mkhalila@outlook.com>
2016-11-06 02:16:08 +0000

    Creates initial main menu with title

--- source_code/view/OwareUI.java
+++ source_code/view/OwareUI.java
@@ -1,6 +1,13 @@
 package view;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -11,6 +18,23 @@ public class OwareUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Oware");
+
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-background-color: #1b2c47");
+
+        Label lblMenuTitle = new Label("Oware");
+        lblMenuTitle.setStyle("-fx-font: 100px Monospaced;" +
+                " -fx-text-fill: white;");
+
+        HBox hbTitlePane = new HBox(lblMenuTitle);
+        hbTitlePane.setAlignment(Pos.CENTER);
+
+        root.setTop(hbTitlePane);
+        root.setAlignment(hbTitlePane, Pos.CENTER);
+
+        Scene mainMenu = new Scene(root, 400, 480);
+        primaryStage.setScene(mainMenu);
+
         primaryStage.show();
     }
 }
