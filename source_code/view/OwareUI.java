6c66ed8c84e3e3ea4d5ac9528b025d9d97696521
Khalil Ahmed <mkhalila@outlook.com>
2016-11-06 00:42:09 +0000

    Creates view package + Declares board UI class

---
+++ source_code/view/OwareUI.java
@@ -0,0 +1,15 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by mkhal on 06/11/2016.
+ */
+public class OwareUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
