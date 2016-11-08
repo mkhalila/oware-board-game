4bb7f8895a6d7b857dbf06e672463e14552411ab
Tania Copocean <tania.copocean@kcl.ac.uk>
2016-11-08 14:31:42 +0000

    Create game object and initialise it

--- source_code/model/Game.java
+++ source_code/model/Game.java
@@ -1,7 +1,20 @@
 package model;
+import java.lang.Math;
 
-/**
- * Created by k1502685 on 08/11/16.
- */
 public class Game {
+    private Board board;
+    private Player player1;
+    private Player player2;
+    private boolean isFinished;
+    private boolean isP1Turn;
+
+    public Game() {
+        board = new Board();
+        player1 = new Player();
+        player2 = new Player();
+        isFinished = false;
+        isP1Turn = Math.random() < 0.5;
+    }
+
+    
 }
