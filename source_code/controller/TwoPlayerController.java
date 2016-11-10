8246169041014fc4931762cfd549255778c6318f
Khalil Ahmed <mkhalila@outlook.com>
2016-11-10 22:16:09 +0000

    Controller constructs 2 players, view and game objects

--- source_code/controller/TwoPlayerController.java
+++ source_code/controller/TwoPlayerController.java
@@ -4,6 +4,7 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import model.Game;
+import model.Player;
 import view.OwareUI;
 
 /**
@@ -16,5 +17,13 @@ public class TwoPlayerController implements EventHandler{
     @Override
     public void handle(Event event) {
         Button source = (Button) event.getSource();
+
+        Player p1 = new Player();
+        Player p2 = new Player();
+        game = new Game(p1, p2);
+
+        for (int i : game.validHouses()) {
+            System.out.println(i);
+        }
     }
 }
