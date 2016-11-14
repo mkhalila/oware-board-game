5b22231f1e8533a6710555d05d6883b55b64d890
Tania Copocean <tania.copocean@kcl.ac.uk>
2016-11-14 12:51:28 +0000

    Javadoc added for AIPlayer class

--- source_code/model/AIPlayer.java
+++ source_code/model/AIPlayer.java
@@ -3,14 +3,27 @@ package model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-
+/**
+ * A class that describes how an AI Player would chose its next move
+ */
 public class AIPlayer extends Player{
 
 	@Override
+	/**
+	 * A method that chooses the house for an AI player, by considering both how many 
+	 * seeds the player would capture with a specific move and what possibilities this
+	 * would offer to the opponent. 
+	 * Then, assuming this move will be made, it 
+	 * 
+	 * @param validHouses a list of the houses that are valid to make a move from
+	 * @return the house selected
+	 */
 	public int nextMove(ArrayList<Integer> validHouses) {
 		int max = Integer.MIN_VALUE;
 		int house = validHouses.get(0);
 		
+		/* loops through all the valid moves and for each 
+			calculates the number of seeds that would be captured. */
 		for(int i = 0; i < validHouses.size(); ++i) {
 			Game fakeGame = new Game(game);
 			
@@ -19,6 +32,8 @@ public class AIPlayer extends Player{
 			int scoreAfter = getScore();
 			
 			int maxOpponent = 0;
+			
+			/*calculates the best move that the opponent could do in this case*/
 			for(int j = 0; j < fakeGame.validHouses().size(); ++j) {
 				int previousOpponentScore = fakeGame.getPlayer1().getScore();
 				Game fakeOpponentGame = new Game(fakeGame);
@@ -33,6 +48,9 @@ public class AIPlayer extends Player{
 				
 			}
 			
+			/* calculates a difference between how the scores will look like after doing
+			 * this move and finds the maximum
+			 */
 			int difference = scoreAfter - previousScore - maxOpponent;
 			if(difference > max) {
 				max = difference;
@@ -43,23 +61,6 @@ public class AIPlayer extends Player{
 
 		return house;
 	}
-	//This bit was just to test if the AI Player is working
 	
-	public static void main(String[] args) {
-		HumanPlayer player1 = new HumanPlayer();
-		AIPlayer player2 = new AIPlayer();
-		Game game = new Game(player1, player2);
-		
-		ArrayList<Integer> player1Houses = new ArrayList<Integer>(Collections.nCopies(6,1));
-		player1Houses.set(4,0);
-		//player1Houses.set(5,0);
-		
-		ArrayList<Integer> player2Houses = new ArrayList<Integer>(Collections.nCopies(6,4));
-		
-		game.getBoard().setPlayer1(player1Houses);
-		game.getBoard().setPlayer2(player2Houses);
-		
-		System.out.println(player2.nextMove(game.validHouses()));
-	}
 
 }
