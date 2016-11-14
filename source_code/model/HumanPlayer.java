20d0109ace7b873015bf68845820ae260603f20e
DavidThomas <david.thomas@kcl.ac.uk>
2016-11-14 13:55:15 +0000

    Added javadoc

--- source_code/model/HumanPlayer.java
+++ source_code/model/HumanPlayer.java
@@ -3,9 +3,19 @@ package model;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * A class representing a human player for a game of Owari
+ * @author David
+ *
+ */
+
 public class HumanPlayer extends Player {
 
-	
+	/**
+	 * A method that determines the next move to be made (for console version)
+	 * @param validHouses the houses that a player can select
+	 * @return the index associated with the house that the player selects
+	 */
 	public int nextMove(ArrayList<Integer> validHouses) {
 		Scanner sc = new Scanner(System.in);
 		int move = sc.nextInt();
--- source_code/model/RandomPlayer.java
+++ source_code/model/RandomPlayer.java
@@ -2,8 +2,19 @@ package model;
 
 import java.util.ArrayList;
 
+/**
+ * A class representing a player that makes random moves in game of Owari
+ * @author David
+ *
+ */
+
 public class RandomPlayer extends Player {
 
+	/**
+	 * Method that determines the next move to be played by the player
+	 * @param validHouses the houses which the player can select
+	 * @return an index representing the house which will be selected
+	 */
 	public int nextMove(ArrayList<Integer> validHouses){
 		int randomMode = (int)(Math.random() * validHouses.size());
 		System.out.println("Random player: " + validHouses.get(randomMode));
