812ef97b61fd4f3b51e2fa2f5c3e19f23abbf742
Khalil Ahmed <mkhalila@outlook.com>
2016-11-14 14:27:22 +0000

    Commented and JDoc'd MainMenu class

--- source_code/view/MainMenu.java
+++ source_code/view/MainMenu.java
@@ -14,52 +14,84 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
- * Created by mkhal on 12/11/2016.
+ * Type of Scene that represents the Main Menu of the game with a title and 3 game mode buttons
  */
 public class MainMenu extends Scene {
+    //Label containing title of the game
     private Label lblMenuTitle;
+    //Holder of the label title
     private HBox hbTitlePane;
+    //Vertical box for holding menu buttons
     private VBox vbMenuButtons;
+    //Menu Button for two player mode
     private MenuButton btnTwoPlayer;
+    //Menu button for random player mode
     private MenuButton btnRandom;
+    //Menu button for AI player mode
     private MenuButton btnAI;
 
+    /**
+     * Creates Main Menu scene containing title and menu buttons
+     * @param width Initial width of the scene
+     * @param height Initial height of the scene
+     */
     public MainMenu(double width, double height) {
+        //Creates a Scene with a BorderPane as root container
         super(new BorderPane(), width, height);
 
         BorderPane root = (BorderPane) getRoot();
 
+        //Sets BG colour of scene
         root.setStyle("-fx-background-color: #1b2c47");
 
+        //Creates styled title
         lblMenuTitle = new Label("Oware");
         lblMenuTitle.setStyle("-fx-font: 100px Monospaced;" +
                 " -fx-text-fill: white;");
 
+        //Adds label title to container
         hbTitlePane = new HBox(lblMenuTitle);
         hbTitlePane.setAlignment(Pos.CENTER);
 
+        //Add title to top of scene
         root.setTop(hbTitlePane);
         root.setAlignment(hbTitlePane, Pos.CENTER);
 
+        //Creates container for Menu Buttons
         vbMenuButtons = new VBox(10);
         vbMenuButtons.setAlignment(Pos.CENTER);
 
+        //Creates 3 menu buttons
         btnTwoPlayer = new MenuButton("2 Player");
         btnRandom = new MenuButton("Random Player");
         btnAI = new MenuButton("AI Player");
 
+        //Adds buttons to container
         vbMenuButtons.getChildren().addAll(btnTwoPlayer, btnRandom, btnAI);
 
+        //Adds button container to scene
         root.setCenter(vbMenuButtons);
     }
 
+    /**
+     * Allows access to two player button
+     * @return The Menu two player button
+     */
     public Button getBtnTwoPlayer() {
         return btnTwoPlayer;
     }
 
+    /**
+     * Allows access to random player button
+     * @return The Menu random player button
+     */
     public Button getBtnRandom() {
         return btnRandom;
     }
 
+    /**
+     * Allows access to AI player button
+     * @return The Menu AI player button
+     */
     public Button getBtnAI() { return btnAI; }
 }
